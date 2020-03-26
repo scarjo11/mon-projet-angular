df1f78f0aa6dd6bd7d3e36b3e67d1466344b2f71
scarjo11 <sezzle-wave@live.fr>
2020-03-26 21:10:10 +0100

    Implémentation des Services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {rejects} from "assert";
+import {AppareilService} from "./services/appareil.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'mon-projet-angular';
+export class AppComponent implements OnInit{
+  isAuth = false;
+
+  lastUpdate = new Promise(
+    (resolve, reject) => {
+      const date = new Date();
+      setTimeout(
+        () => {
+          resolve(date);
+        }, 2000
+      );
+    }
+  )
+
+  appareils: any[];
+
+  constructor(private appareilService: AppareilService) {
+    setTimeout(
+      () => {
+        this.isAuth = true;
+      }, 4000
+    );
+  }
+
+  ngOnInit() {
+    this.appareils = this.appareilService.appareils;
+  }
+
+  onAllumer(){
+    this.appareilService.switchOnAll();
+  }
+
+  onEteindre(){
+    this.appareilService.switchOffAll();
+  }
 }
---
+++ src/app/appareil/appareil.component.ts
@@ -0,0 +1,41 @@
+import { Component, Input, OnInit } from '@angular/core';
+import {AppareilService} from "../services/appareil.service";
+
+@Component({
+  selector: 'app-appareil',
+  templateUrl: './appareil.component.html',
+  styleUrls: ['./appareil.component.scss']
+})
+export class AppareilComponent implements OnInit {
+
+  @Input() appareilName: string;
+  @Input() appareilStatus: string;
+  @Input() indexOfAppareil: number;
+
+  constructor(private appareilService: AppareilService) { }
+
+  ngOnInit(): void {
+  }
+
+  getStatus() {
+    return this.appareilStatus;
+  }
+
+  getColor() {
+    if(this.appareilStatus === 'allumé') {
+      return 'green';
+    }
+    else if(this.appareilStatus === 'éteint') {
+      return 'red';
+    }
+  }
+
+  onSwitchOnOne(){
+    this.appareilService.switchOnOne(this.indexOfAppareil);
+  }
+
+  onSwitchOffOne(){
+    this.appareilService.switchOffOne(this.indexOfAppareil);
+  }
+
+}
